09517572ec719513e6c873a852255a3350f20214
akkaliyar <ajay@whiz-solutions.com>
2025-09-09 15:37:59 +0530

    push db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -103,7 +103,7 @@ import { DataSource } from 'typeorm';
           port: parseInt(process.env.DB_PORT) || 3306,
           username: process.env.DB_USERNAME || 'root',
           password: process.env.DB_PASSWORD || '',
-          database: process.env.DB_NAME || 'ai_crm',
+          database: process.env.DB_NAME || 'ai_crm_demo',
         };
       },
     }),
